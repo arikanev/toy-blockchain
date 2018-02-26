934be51e0f20af392acaf421a50b5e38b25aa212
Arikanev <arikanevsky@Aris-MacBook-Air.local>
2018-02-26 17:48:33 -0500

    toy implementation

---
+++ block.py
@@ -0,0 +1,89 @@
+import time, hashlib
+
+
+class Block:
+
+    def __init__(self, index, previousHash, timestamp, data, currentHash):
+        self.index = index
+        self.previousHash = previousHash
+        self.timestamp = timestamp
+        self.data = data
+        self.currentHash = currentHash
+
+def getGenesisBlock():
+    return Block(0, '0', '1496518102.896031', "firstBlock",
+                 '0q23nfa0se8fhPH234hnjldapjfasdfansdf23')
+
+blockchain = [getGenesisBlock()]
+
+def calculateHash(index, previousHash, timestamp, data):
+    value = str(index) + str(previousHash) + str(timestamp) + str(data)
+    sha = hashlib.sha256(value.encode('utf-8'))
+    return str(sha.hexdigest())
+
+def calculateHashForBlock(block):
+    return calculateHash(block.index, block.previousHash, block.timestamp,
+                         block.data)
+
+def getLatestBlock():
+    return blockchain[len(blockchain) - 1]
+
+def generateNextBlock(blockData):
+    previousBlock = getLatestBlock()
+    print(previousBlock.currentHash)
+    nextIndex = previousBlock.index + 1
+    nextTimestamp = time.time()
+    nextHash = calculateHash(nextIndex, previousBlock.currentHash,
+                             nextTimestamp, blockData)
+    return Block(nextIndex, previousBlock.currentHash,
+                 nextTimestamp, blockData, nextHash)
+
+# check to see if block properties match
+
+def isSameBlock(block1, block2):
+    if block1.index != block2.index:
+        return False
+    elif block1.previousHash != block2.previousHash:
+        return False
+    elif block1.timestamp != block2.timestamp:
+        return False
+    elif block1.data != block2.data:
+        return False
+    elif block1.currentHash != block2.currentHash:
+        return False
+    return True
+
+    # check to see if newly generate block has correct
+    # properties based on comparison to prev block
+
+def isValidNewBlock(newBlock, previousBlock):
+    if previousBlock.index + 1 != newBlock.index:
+        print('indices arent correct')
+        return False
+    elif previousBlock.currentHash != newBlock.previousHash:
+        print('prev hash does not match')
+        return False
+    elif calculateHashForBlock(newBlock) != newBlock.currentHash:
+        print("invalid hash")
+        return False
+    return True
+
+def isValidChain(bcToValidate):
+    if not isSameBlock(bcToValidate[0], getGenesisBlock()):
+        print('Genesis Block Incorrect')
+        return False
+    tempBlocks = [bcToValidate[0]]
+    for i in range(1, len(bcToValidate)):
+        if isValidNewBlock(bcToValidate[i], tempBlocks[i - 1]):
+            tempBlocks.append(bcToValidate[i])
+        else:
+            return False
+    return True
+
+
+
+
+
+
+
+
